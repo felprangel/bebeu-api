864e5324032c70ddbf935dfc77a20fe79f65b7c1
felprangel <feliperangel897@gmail.com>
2025-06-07 16:50:18 -0300

    adiciona migration para a tabela de beber agua

---
+++ database/migrations/2025_06_07_191552_create_water_intake_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('water_intake', function (Blueprint $table) {
+            $table->unsignedInteger('id')->autoIncrement();
+            $table->unsignedInteger('quantity');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('water_intake');
+    }
+};
