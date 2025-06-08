04aaa9785136ff6230cf2f783567dbb936026966
felprangel <feliperangel897@gmail.com>
2025-06-08 07:44:31 -0300

    adiciona rotas

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,12 @@
 <?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [UsersController::class, 'createUser']);
+Route::post('/login', [UsersController::class, 'login']);
+
+Route::middleware(Authenticate::class . ':sanctum')->group(function () {
+    Route::post('/logout', [UsersController::class, 'logout']);
+});
